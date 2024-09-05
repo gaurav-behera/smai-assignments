b7e067086c8acf8123120cb0be21b1e6aa25de51
Gaurav <gaurav.behera@research.iiit.ac.in>
2024-09-06 00:25:59 +0530

    [+]k-means optimal cluster size

--- assignments/2/k_means_tasks.py
+++ assignments/2/k_means_tasks.py
@@ -24,5 +24,38 @@ def analyse_2d_clustering_data():
     fig = px.scatter(data, x="x", y="y", title="2D Clustering Data - Clustered", width=800, height=600, color="cluster")
     fig.show()
     
+def wcss_vs_k():
+    data = pd.read_csv(os.path.join(base_dir, "data", "processed", "word-embeddings.csv"), index_col=0)
+    data = data.to_numpy()
+    
+    results = []
+    for k in range(1, 20):
+        kmeans = KMeans(k=k)
+        kmeans.fit(data)
+        results.append({"k": k, "wcss": kmeans.wcss})
+    
+    # plot the results
+    df = pd.DataFrame(results)
+    fig = px.line(df, x="k", y="wcss", title="WCSS vs K for word-embeddings", width=800, height=600)
+    fig.show()
+    
+    with open("wcss_vs_k.json", "w") as f:
+        json.dump(results, f)
 
-analyse_2d_clustering_data()
+def sample_clustering(k):
+    data = pd.read_csv(os.path.join(base_dir, "data", "processed", "word-embeddings.csv"), index_col=0).to_numpy()
+    
+    print("KMeans clustering with k =", k, "on word-embeddings data")
+    kmeans = KMeans(k=k)
+    kmeans.fit(data)
+    cluster_preds = kmeans.predict(data)
+    print("\tFinal WCSS:", kmeans.wcss.round(2))
+    print("\tPoints in each cluster:")
+    for i in range(k):
+        print("\t\tCluster", i, ":", np.sum(cluster_preds == i))
+    
+        
+# analyse_2d_clustering_data() 
+# wcss_vs_k()
+# k_kmeans1 = 5
+# sample_clustering(k=k_kmeans1)
